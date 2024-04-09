7f18f451fd4dab335871f0d2b13d5365c056a4ae
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2024-04-09 23:32:09 +1000

    Fix up incorrect pub use usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod utils;
 #[cfg(all(target_arch = "wasm32", feature = "http"))]
 pub mod http;
+#[cfg(all(target_arch = "wasm32", feature = "http"))]
 pub use http::HttpStore;
 #[cfg(all(target_arch = "wasm32", feature = "js_binding"))]
 pub use http::js_binding::WasmHttpStore;
